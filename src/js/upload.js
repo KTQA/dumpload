4da4b5b86da551db57af178c077d0bcb2392ad77
Sam Mulvey <sam@ktqa.org>
2022-02-07 21:00:52 -0800

    no filesize limit for dropzone

--- src/js/upload.js
+++ src/js/upload.js
@@ -2,7 +2,7 @@ Dropzone.autoDiscover = false;
 console.log("upload.js loaded");
 
 document.addEventListener("DOMContentLoaded", function() {
-	let dz = new Dropzone("#upload");
+	let dz = new Dropzone("#upload", { maxFilesize: 0 });
 	dz.on("success", function(file) {
 		console.log("File Succeeded!", file);
 		reload_table();
